086c1390fd19e8ec2a3ffac2680d1b5e9d720aa4
DaoNgocAnh25092004 <161846189+DaoNgocAnh25092004@users.noreply.github.com>
2026-09-26 14:09:12 +0700

    build(config): configure recommendation path aliases

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,20 +1,21 @@
 // @type {import("jest").Config}
 module.exports = {
-  rootDir: ".",
-  testEnvironment: "node",
-  testRegex: ".*\\.spec\\.ts$",
-  moduleFileExtensions: ["ts", "js", "json"],
-  clearMocks: true,
-  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.spec.ts", "!src/main.ts"],
-  transform: {
-    "^.+\\.ts$": [
-      "ts-jest",
-      {
-        tsconfig: "<rootDir>/tsconfig.spec.json",
-      },
-    ],
-  },
-  moduleNameMapper: {
-    "^@common/(.*)$": "<rootDir>/../../packages/common/$1",
-  },
+    rootDir: '.',
+    testEnvironment: 'node',
+    testRegex: '.*\\.spec\\.ts$',
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    clearMocks: true,
+    collectCoverageFrom: ['src/**/*.ts', '!src/**/*.spec.ts', '!src/main.ts'],
+    transform: {
+        '^.+\\.ts$': [
+            'ts-jest',
+            {
+                tsconfig: '<rootDir>/tsconfig.spec.json',
+            },
+        ],
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '^@common/(.*)$': '<rootDir>/../../packages/common/$1',
+    },
 };
